e1ec1d81377e8957b20cf06ddc76e501b0bd9907
Mgen <m@mgenware.com>
2023-10-07 23:47:32 +0800

    Fix failing tests

--- local_node_test.go
+++ local_node_test.go
@@ -13,48 +13,19 @@ func init() {
 }
 
 func TestLocalRunSync(t *testing.T) {
-	output, err := localNode.RunSyncUnsafe("echo abc")
+	output, err := localNode.RunCmdSync("", "echo abc")
 	assert.NoError(t, err)
 	assert.Equal(t, "abc\n", string(output))
 }
 
 func TestLocalRun(t *testing.T) {
-	err := localNode.RunUnsafe("echo", "abc")
+	err := localNode.RunCmd("", "echo", "abc")
 	assert.NoError(t, err)
 }
 
 func TestLocalRunSyncWithError(t *testing.T) {
-	output, err := localNode.RunSyncUnsafe("cat ./__not_exist__")
+	output, err := localNode.RunCmdSync("", "cat ./__not_exist__")
 	// Output should not be empty even if error happened
 	assert.Equal(t, string(output) != "", true)
 	assert.Error(t, err)
-}
-
-func TestLocalRunCD(t *testing.T) {
-	err := localNode.CDUnsafe("/")
-	if err != nil {
-		panic(err)
-	}
-	output := runSync(localNode, "pwd")
-	assert.Equal(t, "/\n", string(output))
-}
-
-func TestLocalLastDir(t *testing.T) {
-	err := localNode.CDUnsafe("/")
-	if err != nil {
-		panic(err)
-	}
-	output := runSync(localNode, "pwd")
-	assert.Equal(t, "/\n", string(output))
-	// Double check if last dir is kept on subsequent commands
-	output = runSync(localNode, "pwd")
-	assert.Equal(t, "/\n", string(output))
-}
-
-func runSync(node *LocalNode, cmd string) string {
-	output, err := node.RunSyncUnsafe(cmd)
-	if err != nil {
-		panic(err)
-	}
-	return string(output)
 }
